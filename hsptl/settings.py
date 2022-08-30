204731abf5f9e72f049be0abdaf169bbad85a140
Pranavnair143 <prnavnair2001@gmail.com>
2022-08-30 12:33:27 +0530

    update_static_folder_path

--- hsptl/settings.py
+++ hsptl/settings.py
@@ -117,12 +117,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
